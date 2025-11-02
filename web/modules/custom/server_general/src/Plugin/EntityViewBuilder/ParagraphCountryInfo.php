13bc6fa814637feb68d28d789d32c93279c34a96
Amitai Burstein <amitai@gizra.com>
2025-11-02 19:52:06 +0200

    Cleanups

--- web/modules/custom/server_general/src/Plugin/EntityViewBuilder/ParagraphCountryInfo.php
+++ web/modules/custom/server_general/src/Plugin/EntityViewBuilder/ParagraphCountryInfo.php
@@ -5,11 +5,14 @@ declare(strict_types=1);
 namespace Drupal\server_general\Plugin\EntityViewBuilder;
 
 use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Core\Url;
 use Drupal\node\NodeInterface;
 use Drupal\paragraphs\ParagraphInterface;
 use Drupal\pluggable_entity_view_builder\EntityViewBuilderPluginAbstract;
 use Drupal\server_general\ProcessedTextBuilderTrait;
 use Drupal\server_general\ThemeTrait\CountryThemeTrait;
+use Drupal\server_general\ThemeTrait\Enum\FontSizeEnum;
+use Drupal\server_general\ThemeTrait\LinkThemeTrait;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -24,6 +27,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class ParagraphCountryInfo extends EntityViewBuilderPluginAbstract {
 
   use CountryThemeTrait;
+  use LinkThemeTrait;
   use ProcessedTextBuilderTrait;
 
   /**
@@ -59,11 +63,44 @@ class ParagraphCountryInfo extends EntityViewBuilderPluginAbstract {
     $current_group = $this->ogContext->getGroup();
 
     if (!$current_group) {
-      // No group context, don't render.
+      // No group context, just indicate that user needs to navigate into the
+      // correct domain.
+      $elements = [];
+
+      $element = [
+        $this->wrapTextResponsiveFontSize($this->t('News Placeholder'), FontSizeEnum::ThreeXl),
+        $this->wrapTextItalic($this->t('Navigate to the a country site to see this information')),
+      ];
+
+      $elements[] = $this->wrapContainerVerticalSpacing($element);
+
+      // Add list of all country links.
+      $country_links = $this->getAllCountryUrls();
+      if (!empty($country_links)) {
+        $links_element = $this->wrapContainerVerticalSpacing($country_links);
+
+        // Add cache tags for all country nodes.
+        $cache = CacheableMetadata::createFromRenderArray($links_element);
+        $cache->addCacheTags(['node_list:country']);
+        $cache->applyTo($links_element);
+
+        $elements[] = $links_element;
+      }
+
+      $elements = $this->wrapContainerVerticalSpacing($elements);
+      $elements = $this->wrapContainerWide($elements);
+
+      $cache = CacheableMetadata::createFromRenderArray($elements);
+      $cache->addCacheableDependency($current_group);
+      $cache->addCacheContexts(['url.site']);
+      $cache->applyTo($elements);
+
+      $build[] = $elements;
+
       return $build;
     }
 
-    if (!$current_group instanceof NodeInterface || $current_group->bundle() !== 'country') {
+    if ($current_group->getEntityTypeId() !== 'node' || $current_group->bundle() !== 'country') {
       // Not a Country group, don't render.
       return $build;
     }
@@ -84,6 +121,60 @@ class ParagraphCountryInfo extends EntityViewBuilderPluginAbstract {
     $build[] = $element;
 
     return $build;
+  }
+
+  /**
+   * Get all country URLs.
+   *
+   * Returns an array of country URLs, using the first hostname from
+   * for each published Country node.
+   *
+   * @return array
+   *   Array of render arrays for country links, keyed by country title.
+   */
+  protected function getAllCountryUrls(): array {
+    $storage = $this->entityTypeManager->getStorage('node');
+
+    // Query all published Country nodes.
+    $query = $storage->getQuery()
+      ->condition('type', 'country')
+      ->condition('status', NodeInterface::PUBLISHED)
+      ->accessCheck(TRUE)
+      ->sort('title', 'ASC');
+
+    $nids = $query->execute();
+
+    if (empty($nids)) {
+      return [];
+    }
+
+    $countries = $storage->loadMultiple($nids);
+    $country_links = [];
+
+    foreach ($countries as $country) {
+      // Get the first hostname from field_host_names.
+      if ($country->get('field_hostnames')->isEmpty()) {
+        continue;
+      }
+
+      $hostname = $country->get('field_hostnames')->first()->value;
+      if (empty($hostname)) {
+        continue;
+      }
+
+      // If hostname has `ddev.site` then add 4443 port for local dev.
+      if (str_ends_with($hostname, 'ddev.site')) {
+        $hostname .= ':4443';
+      }
+
+      // Build URL using the hostname.
+      $url = Url::fromUri('https://' . $hostname);
+
+      // Build link element.
+      $country_links[] = $this->buildLink($country->label(), $url);
+    }
+
+    return $country_links;
   }
 
 }
